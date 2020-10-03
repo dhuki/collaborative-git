efd77ac20c84bdf95297a7d851edea7e1de35dd8
dhuki <dhukidwir@gmail.com>
2020-10-03 12:39:26 +0700

    perbaikan

--- common/cons.go
+++ common/cons.go
@@ -8,8 +8,3 @@ const ENV_PATH_TESTING = "C:\\Users\\pricy\\go\\src\\github.com\\rest-template\\
 // const for resonse success
 const RESPONSE_SUCCESS = true
 const RESPONSE_MSG_SUCCESS = "Success"
-
-// const for request model
-const (
-	ContextKeyRequestModel contextKey = iota
-)
